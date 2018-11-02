aad2eab3b9394bd538d59be9a193561903ae45c9
Ariel Bridgeman <ariel.bridgeman@gmail.com>
2018-11-02 10:41:30 +0100

    updated sampler to sample only once per page and not count/save machine signature areas currently. assumes mach_sig and hw element do not overlap

--- sample_pages.py
+++ sample_pages.py
@@ -3,6 +3,7 @@ import cv2
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import re
 # import IPython
 from collections import defaultdict
 
@@ -10,7 +11,7 @@ tuner = defaultdict(list)
 save_dir = "../data/samples/"
 
 
-def random_crop(id, dims, img, mask="", samples=10, box=100, side=15):
+def random_crops(id, dims, img, masks, samples=5, box=150, side=20, keep_mach_sigs=0):
     """
     Create x random selections of a page with a fixed box size
 
@@ -20,7 +21,7 @@ def random_crop(id, dims, img, mask="", samples=10, box=100, side=15):
 
     img: imread object of img (df["path"])
 
-    mask: imread object of mask (df["mask"])
+    masks: array with imread object of masks (df["mask"])
 
     samples : number of times to sample from a page;
              should depend some on page size, no?
@@ -36,85 +37,106 @@ def random_crop(id, dims, img, mask="", samples=10, box=100, side=15):
 
     """
 
+    # TO DO change sample if dims halved of image....
     h, w = dims
-
-    for it in range(samples):
+    it = 0
+    while it < samples:
         # - bottom left pixel of box -
         rand_h = np.random.randint(0, h - box)
         rand_w = np.random.randint(0, w - box)
 
         crop_img = img[rand_h:rand_h + box, rand_w:rand_w + box]
-
-        if mask == "":
+        if isinstance(masks, str):
+            # to do, make saving out an option, otherwise return matrix
             cv2.imwrite(save_dir + "no_mask/%s_%i.png" % (pageid, it), crop_img)
+            it += 1
 
-        # TO DO apply cropped mask and variance threshold (NEED TO STUDY)
-        # or keep within center of box with side modifications
-        # TO TUNE FURTHER; about 1 out of every 500 false positive
-        # typically pure white or close to it
-        # threhold value of 60 helps but the passing ones
-        # range from 300-700 positive pixels which are in line with positives
+        # page has mask(s)
         else:
+            # TO DO apply cropped mask and variance threshold (NEED TO STUDY)
+            # or keep within center of box with side modifications
+            # TO TUNE FURTHER
 
-            crop_mask = np.array(mask[rand_h:rand_h + box,
-                                      rand_w:rand_w + box])
-            mask_img = cv2.bitwise_and(crop_img, crop_img, mask=crop_mask)
+            hasMask = 0
+            # this implementation would allow HW element near a machine element
+            # does not distinguish between mark/text mixed cases
+            for hwType, mask in masks.items():
+                crop_mask = np.array(mask[rand_h:rand_h + box,
+                                          rand_w:rand_w + box])
+                whites = sum(crop_mask.flatten() == 255)
 
-            whites = sum(mask_img.flatten() == 0)
+                # has indent to ensure HW element (label not on edge)
+                sub_mask = np.array(mask[rand_h + side:rand_h + box - side,
+                                         rand_w + side:rand_w + box - side])
 
-            # has indent
-            sub_mask = np.array(mask[rand_h + side:rand_h + box - side,
-                                     rand_w + side:rand_w + box - side])
+                sub_whites = sum(sub_mask.flatten() == 255)
 
-            # TO DO decide on threshold value or use any to expedite search
-            sub_whites = sum(sub_mask.flatten() == 255)
+                if sub_whites > 0:
+                    mask_img = cv2.bitwise_and(crop_img, crop_img,
+                                               mask=crop_mask)
+                    plt.imshow(mask_img)
+                    plt.show()
 
-            if sub_whites > 0:
+                    if hwType == "mach_sig":
 
-                # look at image
-                # plt.imshow(crop_img)
-                # plt.show()
+                        if keep_mach_sigs:
+                            cv2.imwrite(save_dir + "mask/%s/%s_%i.png" %
+                                        (hwType, pageid, it), crop_img)
+                            it += 1
+                            break
 
-                # look at filtered image
-                plt.imshow(mask_img)
-                plt.show()
+                        # don't want to save these; don't count this sample
+                        else:
+                            # continue as may have other masks it works with
+                            continue
 
-                tuner["sum_whites_sub"].append(sub_whites)
-                tuner["sum_whites"].append(whites)
+                    # assumed either text or mark; save out & new sample
+                    else:
+                        save_path = save_dir + "mask/%s/%s_%i.png" % \
+                            (hwType, pageid, it)
 
-                # excludes mask
-                mean, stdDev = cv2.meanStdDev(crop_img, mask=crop_mask)
+                        cv2.imwrite(save_path, crop_img)
 
-                tuner["mean_r"].append(mean[0])
-                tuner["mean_g"].append(mean[1])
-                tuner["mean_b"].append(mean[2])
+                        tuner["hwType"].append(hwType)
+                        tuner["whites_sub_mask"].append(sub_whites)
+                        tuner["whites_mask"].append(whites)
+                        # tuner["nonblacks_mask_img"].append()
 
-                tuner["std_r"].append(stdDev[0])
-                tuner["std_g"].append(stdDev[1])
-                tuner["std_b"].append(stdDev[2])
+                        # excludes mask
+                        mean, stdDev = cv2.meanStdDev(crop_img, mask=crop_mask)
+                        tuner["mean_r"].append(mean[0][0])
+                        tuner["mean_g"].append(mean[1][0])
+                        tuner["mean_b"].append(mean[2][0])
 
-                # includes mask elements
-                mean2, stdDev2 = cv2.meanStdDev(crop_img)
+                        tuner["std_r"].append(stdDev[0][0])
+                        tuner["std_g"].append(stdDev[1][0])
+                        tuner["std_b"].append(stdDev[2][0])
 
-                tuner["mean_r_bkg"].append(mean2[0])
-                tuner["mean_g_bkg"].append(mean2[1])
-                tuner["mean_b_bkg"].append(mean2[2])
+                        # includes mask elements
+                        mean2, stdDev2 = cv2.meanStdDev(crop_img)
 
-                tuner["std_r_bkg"].append(stdDev2[0])
-                tuner["std_g_bkg"].append(stdDev2[1])
-                tuner["std_b_bkg"].append(stdDev2[2])
+                        tuner["mean_r_bkg"].append(mean2[0][0])
+                        tuner["mean_g_bkg"].append(mean2[1][0])
+                        tuner["mean_b_bkg"].append(mean2[2][0])
 
-                path = save_dir + "hw_mask/in_samp/%s_%i.png" % (pageid, it)
-                tuner["path"].append(path)
+                        tuner["std_r_bkg"].append(stdDev2[0][0])
+                        tuner["std_g_bkg"].append(stdDev2[1][0])
+                        tuner["std_b_bkg"].append(stdDev2[2][0])
 
-                tuner["pen"].append(input('mark y / n?'))
-                tuner["char"].append(input('char(s) y / n?'))
-                tuner["edge"].append(input('on edge y / n?'))
+                        tuner["path"].append(save_path)
 
-                cv2.imwrite(path, crop_img)
-            else:
-                cv2.imwrite(save_dir + "hw_mask/not_in_samp/%s_%i.png" %
-                            (pageid, it), crop_img)
+                        # tuner["pen"].append(input('mark y / n?'))
+                        # tuner["char"].append(input('char(s) y / n?'))
+                        # tuner["edge"].append(input('on edge y / n?'))
+
+                        it += 1
+                        break
+
+                # had mask but not included in crop
+                else:
+                    cv2.imwrite(save_dir + "mask/not_in_samp/%s_%i.png" %
+                                (pageid, it), crop_img)
+                    it += 1
 
 
 data = pd.read_hdf("labels/26-10.hdf")
@@ -123,39 +145,53 @@ data = pd.read_hdf("labels/26-10.hdf")
 sel = data.groupby(["pageid", "hwType"], as_index=False).first()
 
 dims = defaultdict(list)
-for index, row in sel.iterrows():
-    pageid = (row["path"].split(".jpg")[0]).split("/")[2:5]
+for name, group in sel.groupby(["pageid", "path"], as_index=False):
+    # if more than one mask, path will be duplicated
+    path = group["path"].unique()[0]
+
+    # creating unique name to save cropped image out to
+    pageid = (re.sub("img/", "", path).split(".jpg")[0]).split("/")[2:4]
     pageid = "_".join(pageid)
 
+    # print(len(group["mask"].unique()))
+
     # 0 import in image and mask
-    img = cv2.imread(row["path"])
+    img = cv2.imread(path)
     h, w = img.shape[:2]
     dims["height"].append(h)
     dims["width"].append(w)
 
-    # only need to check mask if hasHW
-    if row.hwType != "":
+    # means that no masks are present; hasHW = 0
+    if "" in group["mask"].unique():
+        random_crops(pageid, (h, w), img)
 
-        # comes in inverted from saved png
-        mask = cv2.imread(row["mask"], 0)
-        m_h, m_w = mask.shape[:2]
-        dims["m_h"].append(m_h)
-        dims["m_w"].append(m_w)
-
-        # 1: select random box(es) for CNN
-        random_crop(pageid, (h, w), img, mask)
+    # has mask(s)
     else:
-        random_crop(pageid, (h, w), img)
+        masks = {}
+        # need to iterate over for cropper
+        for index, el in group.iterrows():
+            # comes in inverted from saved png
+            mask = cv2.imread(el["mask"], 0)
+            masks[el["hwType"]] = mask
+        random_crops(pageid, (h, w), img, masks)
 
-    if index % 5 == 0:
-        samples = pd.DataFrame(tuner)
-        samples.to_hdf("threshold.hdf")
-        tuner = defaultdict(list)
-        if index == 10:
-            break
+    #         mask = cv2.imread(row["mask"], 0)
+    #         m_h, m_w = mask.shape[:2]
+    #         dims["m_h"].append(m_h)
+    #         dims["m_w"].append(m_w)
 
-dimensions = pd.DataFrame(dims)
-dimensions.to_hdf("page_dims.hdf")
+    #         # 1: select random box(es) for CNN
+    #         random_crop(pageid, (h, w), img, mask)
+    #     else:
+    #         dims["m_h"].append(0)
+    #         dims["m_w"].append(0)
+    #         random_crop(pageid, (h, w), img)
+
+samples = pd.DataFrame(tuner)
+samples.to_hdf("threshold.hdf", key="data")
+
+# dimensions = pd.DataFrame(dims)
+# dimensions.to_hdf("page_dims.hdf", key="data")
 
 
 # TO DO | 2D histogram of page dimensions
