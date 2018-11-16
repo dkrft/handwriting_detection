88579fd3ae13fcb212b3d86f951c24b2841ac71d
Ariel Bridgeman <ariel.bridgeman@gmail.com>
2018-11-16 19:02:03 +0100

    testing

--- web_app/app.py
+++ web_app/app.py
@@ -1,6 +1,7 @@
 # Load the packages
 
 from flask import Flask, render_template, request
+import os
 
 # Connect the app
 app = Flask(__name__)
@@ -11,6 +12,17 @@ def homepage():
     """Render home page"""
     return render_template('home.html')
 
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    file = request.files['image']
+    f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+
+    # add your custom code to check that the uploaded file is a valid image
+    # and not a malicious file (out-of-scope for this post)
+    file.save(f)
+
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)  # Set to false when deploying
